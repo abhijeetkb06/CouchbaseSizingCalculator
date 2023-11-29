8958efb7b5f5bfa46845702b88a51a2f55f5100b
abhijeetkb06 <106691047+abhijeetkb06@users.noreply.github.com>
2023-11-28 20:54:13 -0600

    updated for layout fix with two new fields

--- src/main/java/org/couchbase/CouchbaseSizer.java
+++ src/main/java/org/couchbase/CouchbaseSizer.java
@@ -10,23 +10,40 @@ public class CouchbaseSizer extends JFrame {
     private JTextField totalNodesField;
     private JLabel ramUsageLabel;
     private JLabel healthLabel;
+    private JTextField desiredRamUsageField; // New field for Desired RAM Usage %
+    private JTextField moreNodesForDesiredRamField; // New field for More Nodes For Desired RAM Usage %
 
     public CouchbaseSizer() {
+
         setTitle("Couchbase Cluster Sizer");
-        setSize(600, 300); // Adjust the size as needed
+//        setSize(600, 300); // Doubled width and height
+
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        setLayout(new GridLayout(9, 6, 15, 20)); // Added horizontal and vertical gaps
+//        setLayout(new FlowLayout((FlowLayout.TRAILING))); // Added horizontal and vertical gaps
+
+        setSize(800, 350); // Adjust the size as needed
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new GridLayout(7, 2, 10, 10)); // Added horizontal and vertical gaps
+//        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS)); // Changed to BoxLayout
+
+        setLayout(new GridLayout(9, 2, 10, 10)); // Changed to GridLayout with 9 rows and 2 columns
 
         add(new JLabel("Currently Used RAM per Node (GB):"));
-        currentlyUsedRamPerNodeField = new JTextField();
+        currentlyUsedRamPerNodeField = new JTextField("236");
+        currentlyUsedRamPerNodeField.setEditable(false); // Make it non-editable
+        currentlyUsedRamPerNodeField.setColumns(10);
         add(currentlyUsedRamPerNodeField);
 
         add(new JLabel("RAM Allocated per Node (GB):"));
-        ramAllocatedPerNodeField = new JTextField();
+        ramAllocatedPerNodeField = new JTextField("256");
+        ramAllocatedPerNodeField.setEditable(false); // Make it non-editable
+        ramAllocatedPerNodeField.setColumns(10);
         add(ramAllocatedPerNodeField);
 
         add(new JLabel("Total Nodes:"));
-        totalNodesField = new JTextField();
+        totalNodesField = new JTextField("36");
+        totalNodesField.setEditable(false); // Make it non-editable
+        totalNodesField.setColumns(10);
         add(totalNodesField);
 
         add(new JLabel("RAM Usage (%):"));
@@ -36,10 +53,30 @@ public class CouchbaseSizer extends JFrame {
 
         ramUsageLabel = new JLabel("90%");
         add(ramUsageLabel);
-
+        // Added space
+        add(new JLabel());
         add(new JLabel("Health:"));
         healthLabel = new JLabel();
         add(healthLabel);
+
+        // New fields directly at the bottom
+
+
+        add(new JLabel("Desired RAM Usage (%):"));
+        desiredRamUsageField = new JTextField();
+        desiredRamUsageField.setColumns(10);
+        add(desiredRamUsageField);
+
+
+
+        add(new JLabel("Add More Nodes For Desired RAM Usage (%):"));
+        moreNodesForDesiredRamField = new JTextField();
+        moreNodesForDesiredRamField.setColumns(10);
+        add(moreNodesForDesiredRamField);
+
+        // Prepopulate the new fields
+//        desiredRamUsageField.setText("50");
+//        moreNodesForDesiredRamField.setText("10");
 
         calculateRamUsage();
     }
@@ -48,9 +85,8 @@ public class CouchbaseSizer extends JFrame {
         try {
             double ramAllocatedPerNode = Double.parseDouble(ramAllocatedPerNodeField.getText());
             int totalNodes = Integer.parseInt(totalNodesField.getText());
-            double targetRamUsage = ramUsageSlider.getValue() / 100.0;
 
-            double currentlyUsedRamPerNode = (ramAllocatedPerNode * totalNodes * targetRamUsage) / totalNodes;
+            double currentlyUsedRamPerNode = (ramAllocatedPerNode * totalNodes * ramUsageSlider.getValue() / 100.0) / totalNodes;
             currentlyUsedRamPerNodeField.setText(String.format("%.2f", currentlyUsedRamPerNode));
 
             ramUsageLabel.setText(ramUsageSlider.getValue() + "%");
